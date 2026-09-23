71abbd91b44c88fc71d1e9b193bbd5ff5f7e4e3c
Reivajh06 <129733350+Reivajh06@users.noreply.github.com>
2026-09-23 09:19:59 +0100

    Person with name attribute

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -2,4 +2,13 @@ package software.ulpgc.katas;
 
 public class Person {
 
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
